d980fdcb1e5267d176abdb1d0b3c6dc1173bbb6e
Jonathan Dorety <kyullog@gmail.com>
2019-11-30 23:54:17 -0800

    Create avatar CRT effect

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const app = new PIXI.Application({
+  width: 300,
+  height: 300
+});
+const picDiv = document.getElementById("avatar");
+picDiv.appendChild(app.view);
+const myFilter = new PIXI.filters.CRTFilter();
+myFilter.noise = 0.4;
+myFilter.noiseSize = 0.1;
+myFilter.lineContrast = 0.75
+myFilter.seed = Math.random();
+myFilter.vignetting = 0.4;
+myFilter.vignettingAlpha = 0.9;
+myFilter.vignettingBlur = 0.5;
+
+const avatar = PIXI.Sprite.from("../assets/avatar.jpg");
+avatar.filters = [myFilter];
+avatar.width = app.screen.width;
+avatar.height = app.screen.height;
+app.stage.addChild(avatar);
+
+app.ticker.add(() => {
+    myFilter.seed = Math.random();
+    myFilter.time = 0.3
+})
+// while (true) {
+//   myFilter.seed = Math.random();
+//   myFilter.time = 0.5
+// }
