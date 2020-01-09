3e230efb8518fbd7c13a428dc67a0be791ff1d79
Jonathan Dorety <kyullog@gmail.com>
2020-01-08 19:28:41 -0800

    Remove gimmick avatar

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 const app = new PIXI.Application({
-  width: 300,
-  height: 300
+  width: 150,
+  height: 150
 });
+
 const picDiv = document.getElementById("avatar");
 picDiv.appendChild(app.view);
 const myFilter = new PIXI.filters.CRTFilter();
@@ -12,11 +13,15 @@ myFilter.vignetting = 0.4;
 myFilter.vignettingAlpha = 0.9;
 myFilter.vignettingBlur = 0.5;
 
-const avatar = PIXI.Sprite.from("../assets/avatar.jpg");
+const avatar = PIXI.Sprite.from("../assets/profile.png");
+const tv = PIXI.Sprite.from("../assets/television-set-retro.jpg")
 avatar.filters = [myFilter];
 avatar.width = app.screen.width;
 avatar.height = app.screen.height;
+// tv.width = app.screen.width;
+// tv.height = app.screen.height;
 app.stage.addChild(avatar);
+// app.stage.addChild(tv);
 
 app.ticker.add(() => {
     myFilter.seed = Math.random();
